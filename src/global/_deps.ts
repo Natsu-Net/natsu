640da27ede4f93ade2e6001154f4a7fe63b16898
Aiko-Suzuki <42787030+Aiko-Suzuki@users.noreply.github.com>
2023-07-08 10:14:23 -0400

    up deps

--- src/global/_deps.ts
+++ src/global/_deps.ts
@@ -1,6 +1,6 @@
-export * as Oak from "https://deno.land/x/oak@v12.5.0/mod.ts";
+export * as Oak from "https://deno.land/x/oak@v12.6.0/mod.ts";
 export * as ink from "https://deno.land/x/ink@1.3/mod.ts"
-export * as fs from "https://deno.land/std@0.159.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.159.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.193.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.193.0/path/mod.ts";
 export * as denodb from "https://raw.githubusercontent.com/Aiko-Suzuki/denodb/master/mod.ts";
 export * as uwu from "https://raw.githubusercontent.com/Aiko-Suzuki/uwu-template/main/mod.ts";
